72c30fb4f95303ea8fc89a5a44552372e0d24e56
Harsha Sandeep Challa <sandeepch259@gmail.com>
2024-08-22 19:07:45 -0400

    final

---
+++ index.js
@@ -0,0 +1,31 @@
+let homeScore = 0,
+  guestScore = 0;
+const homeScoreDisplay = document.getElementById("home-score");
+const guestScoreDisplay = document.getElementById("guest-score");
+
+function reset() {
+  homeScoreDisplay.textContent = homeScore;
+  guestScoreDisplay.textContent = guestScore;
+  homeScoreDisplay.classList.remove("leading");
+  guestScoreDisplay.classList.remove("leading");
+}
+reset();
+
+function addScore(score, el) {
+  if (el.parentNode.parentNode.id === "home") {
+    homeScoreDisplay.textContent = +homeScoreDisplay.textContent + score;
+  } else {
+    guestScoreDisplay.textContent = +guestScoreDisplay.textContent + score;
+  }
+
+  if (+homeScoreDisplay.textContent > +guestScoreDisplay.textContent) {
+    homeScoreDisplay.classList.add("leading");
+    guestScoreDisplay.classList.remove("leading");
+  } else if (+homeScoreDisplay.textContent < +guestScoreDisplay.textContent) {
+    homeScoreDisplay.classList.remove("leading");
+    guestScoreDisplay.classList.add("leading");
+  } else if (+homeScoreDisplay.textContent === +guestScoreDisplay.textContent) {
+    homeScoreDisplay.classList.remove("leading");
+    guestScoreDisplay.classList.remove("leading");
+  }
+}
